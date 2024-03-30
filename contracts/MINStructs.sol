eca00f476b73f7845bcdf3b251552d559305d1fb
Arifcan Balta <baltarifcan@gmail.com>
2024-03-30 20:23:20 +0300

    Add comments and create transfer struct

--- contracts/MINStructs.sol
+++ contracts/MINStructs.sol
@@ -2,25 +2,39 @@
 pragma solidity 0.8.20;
 
 /**
- * @title VestingSchedule
+ * @title MINStructs
+ * @dev A library for managing MIN token related structures.
  */
 library MINStructs {
+    /**
+     * @dev Struct to represent a vesting schedule for MIN tokens.
+     * @param tgePermille The permille (parts per thousand) of the total amount of tokens to be released at Token Generation Event (TGE).
+     * @param beneficiary The address that will receive the tokens after they are released.
+     * @param startTimestamp The start time of the vesting period, in Unix time.
+     * @param cliffDuration The duration of the cliff period, in seconds. No tokens are released during the cliff period.
+     * @param vestingDuration The total duration of the vesting period, in seconds.
+     * @param slicePeriodSeconds The duration of a slice period for the vesting, in seconds. Tokens are released in slices.
+     * @param totalAmount The total amount of tokens to be released by the end of the vesting period.
+     * @param releasedAmount The amount of tokens that have already been released.
+     */
     struct VestingSchedule {
-        // permille of the total amount of tokens to be released at TGE
         uint256 tgePermille;
-        // beneficiary of tokens after they are released
         address beneficiary;
-        // start time of the vesting period
         uint256 startTimestamp;
-        // cliff period in seconds
         uint256 cliffDuration;
-        // duration of the vesting period in seconds
         uint256 vestingDuration;
-        // duration of a slice period for the vesting in seconds
         uint256 slicePeriodSeconds;
-        // total amount of tokens to be released at the end of the vesting
         uint256 totalAmount;
-        // amount of tokens released
         uint256 releasedAmount;
+    }
+
+    /**
+     * @dev Struct to represent a transfer of MIN tokens.
+     * @param to The address to transfer tokens to.
+     * @param amount The amount of tokens to transfer.
+     */
+    struct Transfer {
+        address to;
+        uint256 amount;
     }
 }
