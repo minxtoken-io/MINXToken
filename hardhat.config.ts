5904891cd68a836c74817cb7ced5c0e752d15b7d
Arifcan Balta <baltarifcan@gmail.com>
2024-03-30 19:34:12 +0300

    Add and configure typechain

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,6 +8,10 @@ const MUMBAI_ACCOUNTS = process.env.MUMBAI_ACCOUNTS?.split(' ') || [];
 const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || '';
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v6',
+  },
 };
 
 export default config;
