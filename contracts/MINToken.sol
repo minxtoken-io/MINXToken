179085bbb065e46daa667bfa75d679d5ed05e49a
Arifcan Balta <baltarifcan@gmail.com>
2024-03-30 15:57:57 +0300

    Add NatSpec comments and contract files

--- contracts/MINToken.sol
+++ contracts/MINToken.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+ * @title MINPrivateSwap
+ */
 contract MINToken is ERC20 {
     constructor(uint256 amountToMint) ERC20("MIN Token", "MIN") {
         _mint(msg.sender, amountToMint * 10 ** decimals());
