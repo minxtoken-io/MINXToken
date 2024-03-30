0230da59b111c07c07dea4e676e9d0f1080e78ca
Arifcan Balta <baltarifcan@gmail.com>
2024-03-30 19:36:36 +0300

    Add comment to MIN Token contract

--- contracts/MINToken.sol
+++ contracts/MINToken.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @title MINPrivateSwap
  */
 contract MINToken is ERC20 {
+    /**
+     * @dev Creates the ERC20 MIN Token
+     * @param amountToMint The amount of token that'll be minted.
+     */
     constructor(uint256 amountToMint) ERC20("MIN Token", "MIN") {
         _mint(msg.sender, amountToMint * 10 ** decimals());
     }
