2fc4581336e78ddd9eb80ed0b314ddc4cb4d32b0
Arifcan Balta <baltarifcan@gmail.com>
2024-03-30 14:25:36 +0300

    Add mumbai network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,10 @@ import {HardhatUserConfig} from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'solidity-coverage';
 import '@nomiclabs/hardhat-solhint';
+import 'dotenv/config';
+
+const MUMBAI_ACCOUNTS = process.env.MUMBAI_ACCOUNTS?.split(' ') || [];
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || '';
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
 };
