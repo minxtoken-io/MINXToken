97b018eb36f6d61d1d99ae7cae130acae301f978
Arifcan Balta <baltarifcan@gmail.com>
2024-04-01 05:59:34 +0300

    Update MINVestingBase.sol

--- contracts/utils/MINVestingBase.sol
+++ contracts/utils/MINVestingBase.sol
@@ -5,7 +5,6 @@ import "./MINStructs.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 /**
  * @title MINVestingBase
