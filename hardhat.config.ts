9577d90c7b47fef5258561f9d257aef265fd3b32
Arifcan Balta <baltarifcan@gmail.com>
2024-07-17 04:38:31 +0300

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,12 @@ import 'solidity-coverage';
 import '@nomiclabs/hardhat-solhint';
 import 'dotenv/config';
 import 'solidity-docgen';
-const MUMBAI_ACCOUNTS = process.env.MUMBAI_ACCOUNTS?.split(' ') || [];
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || '';
+const POLYGON_ETHERSCAN_API_KEY = process.env.POLYGON_ETHERSCAN_API_KEY || '';
+const POLYGON_AMOY_ACCOUNTS = process.env.POLYGON_AMOY_ACCOUNTS?.split(' ') || [];
+const POLYGON_AMOY_RPC_URL = process.env.POLYGON_AMOY_RPC_URL || '';
+const POLYGON_MAINNET_ACCOUNTS = process.env.POLYGON_MAINNET_ACCOUNTS?.split(' ') || [];
+const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || '';
+
 const config: HardhatUserConfig = {
   solidity: '0.8.20',
   typechain: {
@@ -20,11 +24,40 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
       accounts: {},
     },
-    mumbai: {
-      url: MUMBAI_RPC_URL,
-      chainId: 80001,
-      accounts: MUMBAI_ACCOUNTS,
+    polygonAmoy: {
+      url: POLYGON_AMOY_RPC_URL,
+      chainId: 80002,
+      accounts: POLYGON_AMOY_ACCOUNTS,
     },
+    polygonMainnet: {
+      url: POLYGON_MAINNET_RPC_URL,
+      chainId: 137,
+      gas: "auto",
+      accounts: POLYGON_MAINNET_ACCOUNTS,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: POLYGON_ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: 'polygonAmoy',
+        chainId: 80002,
+        urls: {
+          apiURL: 'https://api-amoy.polygonscan.com/api',
+          browserURL: 'https://amoy.polygonscan.com',
+        },
+      },
+      {
+        network: 'polygonMainnet',
+        chainId: 137,
+        urls: {
+          apiURL: 'https://api.polygonscan.com/api',
+          browserURL: 'https://polygonscan.com',
+        },
+      },
+    ],
   },
 };
 
